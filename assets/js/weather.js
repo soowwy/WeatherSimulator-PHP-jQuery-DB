98bccc4d8d9b5f33d558283176e657b2b4084614
Krassimir Kolev <soowwy@gmail.com>
2016-10-24 01:24:13 +0300

    Update weather.js

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -2,8 +2,15 @@
  * Created by sowwy on 20.10.16.
  */
 
-
 $(function () {
+
+    one = $('#one');
+    two = $('#two');
+    three = $('#three');                        /*here we store the id's of the pictures so we can use them later. */
+    four = $('#four');
+    five = $('#five');
+    six = $('#six');
+    seven = $('#seven');
 
     $('#save').hide();
     $('#info').hide();
@@ -76,292 +83,101 @@ $(function () {
                 $('#fahrenheit').html(degreeF);
                 $('#day').html(day);
 
-                                                                /*all this for the degree*/
 
-                for (var j = 0; j < response.length; j++) {
-                    if (response[j].day == 'Monday') {
-                        var degreeC = response[j].degreeC;
-                        var degreeF = response[j].degreeF;
-                        $("#one").html(degreeC + "|" + degreeF);
-                    }
-                }
-
-                for (var k = 0; k < response.length; k++) {
-                    if (response[k].day == 'Tuesday') {
-                        var degreeC = response[k].degreeC;
-                        var degreeF = response[k].degreeF;
-                        $("#two").html(degreeC + "|" + degreeF);
-                    }
-                }
-
-                for (var l = 0; l < response.length; l++) {
-                    if (response[l].day == 'Tuesday') {
-                        var degreeC = response[l].degreeC;
-                        var degreeF = response[l].degreeF;
-                        $("#three").html(degreeC + "|" + degreeF);
-                    }
-                }
-
-                for (var m = 0; m < response.length; m++) {
-                    if (response[m].day == 'Tuesday') {
-                        var degreeC = response[m].degreeC;
-                        var degreeF = response[m].degreeF;
-                        $("#four").html(degreeC + "|" + degreeF);
-                    }
-                }
-
-                for (var n = 0; n < response.length; n++) {
-                    if (response[n].day == 'Tuesday') {
-                        var degreeC = response[n].degreeC;
-                        var degreeF = response[n].degreeF;
-                        $("#five").html(degreeC + "|" + degreeF);
-                    }
-                }
-
-                for (var o = 0; o < response.length; o++) {
-                    if (response[o].day == 'Tuesday') {
-                        var degreeC = response[o].degreeC;
-                        var degreeF = response[o].degreeF;
-                        $("#six").html(degreeC + "|" + degreeF);
-                    }
-                }
-
-                for (var p = 0; p < response.length; p++) {
-                    if (response[p].day == 'Tuesday') {
-                        var degreeC = response[p].degreeC;
-                        var degreeF = response[p].degreeF;
-                        $("#seven").html(degreeC + "|" + degreeF);
-                    }
-                }
 
 
                                                                     /*here we make the program change the params on
-                                                                      a click. It's quite long and there is alot of
-                                                                      code repeating but it does the job.*/
-                $('#monday2').on("click", function () {
-                    for(var i = 0; i < response.length; i++) {
+                                                                      a click. */
 
-                        if (response[i].day == 'Monday') {
-                            var name = response[i].name;
-                            var precipitation = response[i].precipitation;
-                            var humidity = response[i].humidity;
-                            var wind = response[i].wind;
-                            var degreeC = response[i].degreeC;
-                            var degreeF = response[i].degreeF;
-                            var day = response[i].day;
-                            $('#town').html(name);
-                            $('#precipitation').html(precipitation);
-                            $('#humidity').html(humidity);
-                            $('#wind').html(wind);
-                            $('#celsius').html(degreeC);
-                            $('#fahrenheit').html(degreeF);
-                            $('#day').html("Mon");
 
-                          // $("#one").html(degreeC + "|" + degreeF);
+                monday = $('#monday2');
+                mondayStr = "Monday";
+                tuesday = $('#tuesday2');
+                tuesdayStr = "Tuesday";
+                wednesday = $('#wednesday2');
+                wednesdayStr = "Wednesday";
+                thursday = $('#thursday2');
+                thursdayStr = "Thursday";
+                friday = $('#friday2');
+                fridayStr = "Friday";
+                saturday = $('#saturday2');
+                saturdayStr = "Saturday";
+                sunday = $('#sunday2');
+                sundayStr = "Sunday";
 
-                            if (name == "Dubai") {
-                                $("#weather").html("Sun");
-                            } else if (name == 'Sofia') {
-                                $("#weather").html("Sun");
-                            } else if (name == "Hollywood") {
-                                $("#weather").html("Sun");
+                sun = "Sun";
+                rain = 'Rain';
+                thunders = "Thunders";
+                snow = "Snow";
+                clouds = "Clouds";
+                partlyCloudy = "Partly cloudy";
+
+
+
+                function test(day, value, one, two, three) {
+                    day.on("click", function () {
+                        for(var i = 0; i < response.length; i++) {
+
+                            if (response[i].day == value) {
+                                var name = response[i].name;
+                                var precipitation = response[i].precipitation;
+                                var humidity = response[i].humidity;
+                                var wind = response[i].wind;
+                                var degreeC = response[i].degreeC;
+                                var degreeF = response[i].degreeF;
+                                var day = response[i].day;
+                                $('#town').html(name);
+                                $('#precipitation').html(precipitation);
+                                $('#humidity').html(humidity);
+                                $('#wind').html(wind);
+                                $('#celsius').html(degreeC);
+                                $('#fahrenheit').html(degreeF);
+                                $('#day').html(value);
+
+                                // $("#one").html(degreeC + "|" + degreeF);
+
+                                if (name == "Dubai") {
+                                    $("#weather").html(one);
+                                } else if (name == 'Sofia') {
+                                    $("#weather").html(two);
+                                } else if (name == "Hollywood") {
+                                    $("#weather").html(three);
+                                }
+
+                                $("#day").show();
                             }
+                        }
+                    });
+                }
 
-                            $("#day").show();
+                test(monday, mondayStr, sun, sun, sun);
+                test(tuesday, tuesdayStr, sun, clouds, partlyCloudy);
+                test(wednesday, wednesdayStr, partlyCloudy, snow, sun);
+                test(thursday,thursdayStr, partlyCloudy, partlyCloudy, partlyCloudy);
+                test(friday, fridayStr, clouds, sun, sun);
+                test(saturday, saturdayStr, rain, sun, rain);
+                test(sunday, sundayStr, thunders, thunders, rain);
+
+
+                                                                        /*a function for changing the degree*/
+
+                function changeDegree(value, number) {
+                    for (var j = 0; j < response.length; j++) {
+                        if (response[j].day == value) {
+                            var degreeC = response[j].degreeC;
+                            var degreeF = response[j].degreeF;
+                            number.html(degreeC + "|" + degreeF);
                         }
                     }
-                });
-                $('#tuesday2').on("click", function () {
-                    for(var i = 0; i < response.length; i++) {
+                }
 
-                        if (response[i].day == 'Tuesday') {
-                            var name = response[i].name;
-                            var precipitation = response[i].precipitation;
-                            var humidity = response[i].humidity;
-                            var wind = response[i].wind;
-                            var degreeC = response[i].degreeC;
-                            var degreeF = response[i].degreeF;
-                            var day = response[i].day;
-                            $('#town').html(name);
-                            $('#precipitation').html(precipitation);
-                            $('#humidity').html(humidity);
-                            $('#wind').html(wind);
-                            $('#celsius').html(degreeC);
-                            $('#fahrenheit').html(degreeF);
-                            $('#day').html("Tue");
-
-                            if (name == "Dubai") {
-                                $("#weather").html("Sun");
-                            } else if (name == 'Sofia') {
-                                $("#weather").html("Clouds");
-                            } else if (name == "Hollywood") {
-                                $("#weather").html("Partly cloudy");
-                            }
-
-                            $("#day").show();
-
-                        }
-                    }
-                });
-                $('#wednesday2').on("click", function () {
-                    for(var i = 0; i < response.length; i++) {
-
-                        if (response[i].day == 'Wednesday') {
-                            var name = response[i].name;
-                            var precipitation = response[i].precipitation;
-                            var humidity = response[i].humidity;
-                            var wind = response[i].wind;
-                            var degreeC = response[i].degreeC;
-                            var degreeF = response[i].degreeF;
-                            var day = response[i].day;
-                            $('#town').html(name);
-                            $('#precipitation').html(precipitation);
-                            $('#humidity').html(humidity);
-                            $('#wind').html(wind);
-                            $('#celsius').html(degreeC);
-                            $('#fahrenheit').html(degreeF);
-                            $('#day').html("Wed");
-
-                            if (name == "Dubai") {
-                                $("#weather").html("Partly cloudy");
-                            } else if (name == 'Sofia') {
-                                $("#weather").html("Snow");
-                            } else if (name == "Hollywood") {
-                                $("#weather").html("Sun");
-                            }
-
-                            $("#day").show();
-
-                        }
-                    }
-                });
-                $('#thursday2').on("click", function () {
-                    for(var i = 0; i < response.length; i++) {
-
-                        if (response[i].day == 'Thursday') {
-                            var name = response[i].name;
-                            var precipitation = response[i].precipitation;
-                            var humidity = response[i].humidity;
-                            var wind = response[i].wind;
-                            var degreeC = response[i].degreeC;
-                            var degreeF = response[i].degreeF;
-                            var day = response[i].day;
-                            $('#town').html(name);
-                            $('#precipitation').html(precipitation);
-                            $('#humidity').html(humidity);
-                            $('#wind').html(wind);
-                            $('#celsius').html(degreeC);
-                            $('#fahrenheit').html(degreeF);
-                            $('#day').html("Thu");
-
-                            if (name == "Dubai") {
-                                $("#weather").html("Partly cloudy");
-                            } else if (name == 'Sofia') {
-                                $("#weather").html("Partly cloudy");
-                            } else if (name == "Hollywood") {
-                                $("#weather").html("Partly cloudy");
-                            }
-
-                            $("#day").show();
-
-                        }
-                    }
-                });
-                $('#friday2').on("click", function () {
-                    for(var i = 0; i < response.length; i++) {
-
-                        if (response[i].day == 'Friday') {
-                            var name = response[i].name;
-                            var precipitation = response[i].precipitation;
-                            var humidity = response[i].humidity;
-                            var wind = response[i].wind;
-                            var degreeC = response[i].degreeC;
-                            var degreeF = response[i].degreeF;
-                            var day = response[i].day;
-                            $('#town').html(name);
-                            $('#precipitation').html(precipitation);
-                            $('#humidity').html(humidity);
-                            $('#wind').html(wind);
-                            $('#celsius').html(degreeC);
-                            $('#fahrenheit').html(degreeF);
-                            $('#day').html("Fri");
-
-                            if (name == "Dubai") {
-                                $("#weather").html("Clouds");
-                            } else if (name == 'Sofia') {
-                                $("#weather").html("Sun");
-                            } else if (name == "Hollywood") {
-                                $("#weather").html("Sun");
-                            }
-
-                            $("#day").show();
-                        }
-                    }
-                });
-                $('#saturday2').on("click", function () {
-                    for(var i = 0; i < response.length; i++) {
-
-                        if (response[i].day == 'Saturday') {
-                            var name = response[i].name;
-                            var precipitation = response[i].precipitation;
-                            var humidity = response[i].humidity;
-                            var wind = response[i].wind;
-                            var degreeC = response[i].degreeC;
-                            var degreeF = response[i].degreeF;
-                            var day = response[i].day;
-                            $('#town').html(name);
-                            $('#precipitation').html(precipitation);
-                            $('#humidity').html(humidity);
-                            $('#wind').html(wind);
-                            $('#celsius').html(degreeC);
-                            $('#fahrenheit').html(degreeF);
-                            $('#day').html("Sat");
-
-                            if (name == "Dubai") {
-                                $("#weather").html("Rain");
-                            } else if (name == 'Sofia') {
-                                $("#weather").html("Sun");
-                            } else if (name == "Hollywood") {
-                                $("#weather").html("Rain");
-                            }
-
-                            $("#day").show();
-
-                        }
-                    }
-                });
-                $('#sunday2').on("click", function () {
-                    for(var i = 0; i < response.length; i++) {
-
-                        if (response[i].day == 'Sunday') {
-                            var name = response[i].name;
-                            var precipitation = response[i].precipitation;
-                            var humidity = response[i].humidity;
-                            var wind = response[i].wind;
-                            var degreeC = response[i].degreeC;
-                            var degreeF = response[i].degreeF;
-                            var day = response[i].day;
-                            $('#town').html(name);
-                            $('#precipitation').html(precipitation);
-                            $('#humidity').html(humidity);
-                            $('#wind').html(wind);
-                            $('#celsius').html(degreeC);
-                            $('#fahrenheit').html(degreeF);
-                            $('#day').html("Sun");
-
-                            if (name == "Dubai") {
-                                $("#weather").html("Thunders");
-                            } else if (name == 'Sofia') {
-                                $("#weather").html("Thunders");
-                            } else if (name == "Hollywood") {
-                                $("#weather").html("Rain");
-                            }
-
-                            $("#day").show();
-
-                        }
-                    }
-                });
+                changeDegree(mondayStr, one);
+                changeDegree(tuesdayStr, two);
+                changeDegree(wednesdayStr, three);
+                changeDegree(thursdayStr, four);
+                changeDegree(fridayStr, five);
+                changeDegree(saturdayStr, six);
+                changeDegree(sundayStr, seven);
 
             }
 
